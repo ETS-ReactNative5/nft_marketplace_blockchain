6f2813c47763ed2bc35b5159b7fe7e8e551868a9
Daniel Magro <djm385@icloud.com>
2022-05-29 06:26:50 -0400

    loading images to github

---
+++ assets/components/index.js
@@ -0,0 +1,5 @@
+import assets from "../assets";
+import { COLORS, SHADOWS, SIZES, FONTS } from "../constants/theme";
+import { NFTData } from "../constants/dummy";
+
+export { assets, COLORS, SHADOWS, SIZES, FONTS, NFTData };
