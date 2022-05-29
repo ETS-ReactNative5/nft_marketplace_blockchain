87d9f73f2b7e39e9a278894e7d1e6d42a6aeb5ac
Daniel Magro <djm385@icloud.com>
2022-05-29 09:39:24 -0400

    finished file structure

---
+++ .expo-shared/src/assets/components/index.js
@@ -0,0 +1,5 @@
+import assets from "../assets";
+import { COLORS, SHADOWS, SIZES, FONTS } from "../assets/constants/theme";
+import { NFTData } from "../assets/constants/dummy";
+
+export { assets, COLORS, SHADOWS, SIZES, FONTS, NFTData };
--- assets/components/index.js
+++
@@ -1,5 +0,0 @@
-import assets from "../assets";
-import { COLORS, SHADOWS, SIZES, FONTS } from "../constants/theme";
-import { NFTData } from "../constants/dummy";
-
-export { assets, COLORS, SHADOWS, SIZES, FONTS, NFTData };
